177e69202909fd6431db33f9d0b47219b02883ab
Ricky Pan <ricky.pan@utp.ac.pa>
2022-05-29 17:21:13 -0500

    Commit de la secante

---
+++ secante.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def secante(data):
+    x0=float(print('Ingrese el limite X0:'))
+    x1=float(print('Ingrese el limite X-1:'))
+    
+    f0=f(x0)
+    f1=f(x1)
+    
+    xr=x0-(f0*(x0-x1))/(f1-f0)
+    xa=xr
+    if xr>x0:
+        x0=xr
+    else:
+        x1=xr
+    
+    while True:
+        f0=f(x0)
+        f1=f(x1)
+        xr=x0-(f0*(x0-x1))/(f1-f0)
+        if xr>x0:
+            x0=xr
+        else:
+            x1=xr
+       
+        xr=x0-(f0*(x0-x1))/(f1-f0)
+        
+        E=abs(xr-xa)/xr*100
+        xa=xr
+        if E<=1:
+            break
+
+    return xr
